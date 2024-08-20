7f0d0410fb7e939c7c6b5afc05e55fc8d1f7dfcb
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-08-20 12:55:09 +0100

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,24 +4,15 @@ number of subscribers for a given subreddit
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
+    """ Returns the number of subs for a subreddit """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "Mozilla/5.0 (compatible; Python script; +http://example.com)"
-    }
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data.get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-    except requests.RequestException:
+            "User-Agent": "v1.0.0"
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-if __name__ == "__main__":
-    subreddit = "programming"
-    print(number_of_subscribers(subreddit))
-
+    res = response.json().get("data")
+    return res.get("subscribers")
