2ba0790fd2a1cf214188fce5247bad5848a251c3
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-09-25 23:39:23 +0100

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,6 +16,6 @@ def number_of_subscribers(subreddit):
             results = response.json().get("data")
             return results.get("subscribers")
         else:
-            return 0
+            return ("ok") 
     except requests.RequestException:
-        return 0
+        return ("ok")
