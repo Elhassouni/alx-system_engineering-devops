b3dc77aa928526bf31422143fc8c3eb1f2f54d88
Ayoub Elhassouni <ayoub.elhassouniii@gmail.com>
2024-09-25 20:49:57 +0100

    Update 1-top_ten.py
    2049

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and prints the titles of the first 10 hot
-   posts for a given subreddit"""
-
+"""
+Queries the Reddit API and prints the titles of the first 10 hot
+posts for a given subreddit.
+"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles"""
-
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
     base_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
     # made limit 9 since index is 0 - 9 gives 10 posts
     headers = {'User-Agent': 'custom-script/1.0'}
 
     try:
-        response = requests.get(base_url, headers=headers,
-                                allow_redirects=False)
+        response = requests.get(base_url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             posts = data['data']['children']
