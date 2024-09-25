ce494e0a6e733961015ddec8af98f195f319b01c
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-09-26 00:11:31 +0100

    edit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,46 +1,13 @@
 #!/usr/bin/python3
-"""
-Queries a Reddit API
-
-IMPORTS: requests
-requests: Send and receive data packets from a URI
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """
-    Queries a valid subreddit and retrives the no of subs
-
-    Args:
-    subreddit(str): A reddit topic
-
-    Return:
-    No of subs a subreddit has
-    """
-    try:
-        url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        response = requests.get(url, allow_redirects=False)
-    except Exception as e:
-        print(f'An exception has occurred: {e}')
+def numberof_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/%7B%7D/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    if response.status_code == 200:
-        resp_json = response.json()
-        no_subs = rsp_json['data']['subscribers']
-        return no_subs
-    elif response.status_code == 301:
-        new_loc = response.headers['Location']
-
-        try:
-            new_resp = requests.get(new_loc)
-        except Exception as e:
-            print(f'An exception has occurred: {e}')
-            return 0
-
-        resp_json = new_resp.json()
-        no_subs = resp_json['data']['subscribers']
-        return no_subs
-
-    else:
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
